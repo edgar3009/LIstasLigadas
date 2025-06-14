caaee41f905a4725bfd6392fc56c261183a0a8fe
edgar3009 <64936648+edgar3009@users.noreply.github.com>
2025-06-14 13:56:23 -0700

    Add project files.

---
+++ SimplementeLigadas/Nodo.cs
@@ -0,0 +1,15 @@
+﻿namespace SimplementeLigadas
+{
+    internal class Nodo
+    {
+        public string Valor { get; set; }
+        public Nodo Enlace { get; set; }
+
+        public Nodo(string valor = "", Nodo enlace = null)
+        {
+            Valor = valor;
+            Enlace = enlace;
+        }
+    }
+}
+
---
+++ SimplementeLigadas/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SimplementeLigadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nodo nodo1 = new Nodo();
+            nodo1.Valor = "Primer Nodo";
+        }
+    }
+}
